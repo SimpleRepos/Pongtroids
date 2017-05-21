b4d469171c69e4347cc5cf3b31139fd151930d35
a <a>
2017-05-20 22:05:31 -0700

    screwing with triangles

--- Pongtroids/Scene_Game.cpp
+++ Pongtroids/Scene_Game.cpp
@@ -2,21 +2,39 @@
 #include "st_SharedState.h"
 #include "cl_GfxFactory.h"
 #include <vector>
-#include "ns_Vertex.h"
+#include <cmath>
 
 namespace {
-  const std::vector<Vertex::Pos4Nrm4Tex2> triVerts = {
-    { DirectX::XMFLOAT4( 0.0f,  0.5f, 0, 1), DirectX::XMFLOAT4(0, 0, -1, 0), DirectX::XMFLOAT2(0.5f, 0.0f) },
-    { DirectX::XMFLOAT4( 0.5f, -0.5f, 0, 1), DirectX::XMFLOAT4(0, 0, -1, 0), DirectX::XMFLOAT2(1.0f, 1.0f) },
-    { DirectX::XMFLOAT4(-0.5f, -0.5f, 0, 1), DirectX::XMFLOAT4(0, 0, -1, 0), DirectX::XMFLOAT2(0.0f, 1.0f) }
+  struct Pos4Tex2 {
+    DirectX::XMFLOAT4 pos;
+    DirectX::XMFLOAT2 texcoord;
   };
+
+  std::vector<Pos4Tex2> generateVerts(int ct) {
+    float radsPerVert = DirectX::XM_2PI / ct;
+
+    std::vector<Pos4Tex2> vv;
+    for(float rads = 0; rads < DirectX::XM_2PI; rads += radsPerVert) {
+      vv.emplace_back(Pos4Tex2{ { 0, 0, 0, 1 }, { 0, 0 } });
+
+      float x = std::sin(rads);
+      float y = std::cos(rads);
+      vv.emplace_back(Pos4Tex2{ { x, y, 0, 1 }, { x, y } });
+    }
+
+    vv.emplace_back(Pos4Tex2{ { 0, 0, 0, 1 }, { 0, 0 } });
+    vv.emplace_back(vv[1]);
+
+    return vv;
+  }
+
 }
 
 Scene_Game::Scene_Game(SharedState& shared) : 
   Scene(shared),
-  vShader(shared.factory.createVShader("../Debug/VertexShader.cso", D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST)),
+  vShader(shared.factory.createVShader("../Debug/VertexShader.cso", D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP)),
   pShader(shared.factory.createPShader("../Debug/PixelShader.cso")),
-  verts(shared.factory.createStaticVertexBuffer(triVerts)),
+  verts(shared.factory.createStaticVertexBuffer(generateVerts(100))),
   tex(shared.factory.createTexture(L"../Assets/checkers.png")),
   cBuffer(shared.factory.createConstantBuffer<CB>())
 {
@@ -31,8 +49,6 @@ Scene_Game::Scene_Game(SharedState& shared) :
   cam.setAspectRatio((float)shared.gfx.VIEWPORT_DIMS.width, (float)shared.gfx.VIEWPORT_DIMS.height);
   cam.setEyePos(0, 0, -5);
   cam.setTargetDir(0, 0, 1);
-
-  xform.scale.y = 2;
 
 }
 
--- Pongtroids/VertexShader.hlsl
+++ Pongtroids/VertexShader.hlsl
@@ -4,13 +4,12 @@ cbuffer cb {
 
 struct VOut {
   float4 pos : SV_POSITION;
-  float2 uv : TEXCOORD;
+  float2 uv  : TEXCOORD;
 };
 
 struct VIn {
   float4 pos : POSITION;
-  float4 norm : NORMAL;
-  float2 uv : TEXCOORD;
+  float2 uv  : TEXCOORD;
 };
 
 VOut main(VIn input) {
